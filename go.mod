6e467863b252f1dbea306bafada18775a2160a7e
tebben <ebben.tim@gmail.com>
2025-01-21 12:11:18 +0100

    move from vector search to trigram matching

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/tebben/geocodeur
 go 1.22.0
 
 require (
-	github.com/anush008/fastembed-go v1.0.0
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/xitongsys/parquet-go v1.6.2
 	github.com/xitongsys/parquet-go-source v0.0.0-20200817004010-026bad9b25d0
@@ -12,24 +11,15 @@ require (
 require (
 	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
 	github.com/apache/thrift v0.14.2 // indirect
-	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.13.1 // indirect
-	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/pierrec/lz4/v4 v4.1.8 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/schollz/progressbar/v2 v2.15.0 // indirect
-	github.com/schollz/progressbar/v3 v3.18.0 // indirect
-	github.com/sugarme/regexpset v0.0.0-20200920021344-4d4ec8eaf93c // indirect
-	github.com/sugarme/tokenizer v0.2.3-0.20230829214935-448e79b1ed65 // indirect
-	github.com/yalue/onnxruntime_go v1.16.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/term v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -9,11 +9,22 @@ import (
 	"sync"
 	"time"
 
-	"github.com/anush008/fastembed-go"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/xitongsys/parquet-go-source/local"
 	"github.com/xitongsys/parquet-go/reader"
 )
+
+var aliases = map[string]string{
+	"'s-Hertogenbosch": "Den Bosch",
+}
+
+var truncations = []string{
+	"Rijksweg",
+}
+
+var TABLEOVERTURE = "overture"
+var TABLESEARCH = "overture_search"
 
 // Define your structure matching the Parquet schema
 type Record struct {
@@ -26,85 +37,92 @@ type Record struct {
 }
 
 func main() {
-	os.Setenv("ONNX_PATH", "/home/time/.local/lib/python3.11/site-packages/onnxruntime/capi/libonnxruntime.so.1.20.1")
-	//createDB()
-	queryDB()
+	command := os.Args[1]
+	if command == "create" {
+		createDB()
+	} else if command == "query" {
+		queryDB(os.Args[2])
+	} else {
+		log.Fatalf("Unknown command")
+	}
 }
 
-func queryDB() {
-	//query := "pettelaarpark 's-Hertogenbosch"
-	query := "van veldekekade"
-	model := getEmbeddingModel()
-	defer model.Destroy()
-
-	vector := createEmbedding(model, query)
-	vectorString := vectorToString(vector)
-
-	// example query: SELECT * FROM items ORDER BY embedding <-> '[3,1,2]' LIMIT 5;
+func queryDB(input string) {
 	pool := getDBPool()
 	defer pool.Close()
-	/*
-		-- We have multiple embeddings for each overture record, we only want the closest one
-		-- this goes from 6ms to 15ms
-		WITH ranked_embeddings AS (
-			SELECT
-				o.id, o.name, o.class, o.subclass, vector,
-				vector <-> $1 AS distance,
-				ROW_NUMBER() OVER (PARTITION BY o.id ORDER BY vector <-> $1) AS rn
-			FROM overture_embeddings e
-			LEFT JOIN overture as o ON o.id = e.id
-		)
-		SELECT id, name, class, subclass, vector, distance
-		FROM ranked_embeddings
-		WHERE rn = 1
-		ORDER BY distance
-		LIMIT 5
-
-
-		SELECT o.id, o.name, o.class, o.subclass, vector, vector <-> $1 AS distance
-		FROM overture_embeddings e
-		LEFT JOIN overture as o ON o.id = e.id
-		ORDER BY vector <-> $1
-		LIMIT 5
-	*/
 
 	timeStart := time.Now()
-	rows, err := pool.Query(context.Background(), `
-		SELECT o.id, o.name, o.class, o.subclass, vector, vector <-> $1 AS distance
-		FROM overture_embeddings e
-		LEFT JOIN overture as o ON o.id = e.id
-		ORDER BY vector <-> $1
-		LIMIT 20
-	`, vectorString)
+
+	query := fmt.Sprintf(`
+	WITH ranked_aliases AS (
+			SELECT
+				a.id,
+				a.name,
+				a.class,
+				a.subclass,
+				b.alias,
+				similarity(b.alias, $1) AS sim,
+				ROW_NUMBER() OVER (PARTITION BY a.id ORDER BY similarity(b.alias, $1) DESC) AS rnk
+			FROM %s a
+			JOIN %s b ON a.id = b.id
+			WHERE b.alias %% $1
+		)
+		SELECT id, name, class, subclass, alias, sim
+		FROM ranked_aliases
+		WHERE rnk = 1
+		ORDER BY sim desc
+		LIMIT 10;`, TABLEOVERTURE, TABLESEARCH)
+
+	rows, err := pool.Query(context.Background(), query, input)
 	if err != nil {
 		log.Fatalf("Failed to query database: %v", err)
 	}
 	defer rows.Close()
 	timeEnd := time.Now()
 
-	fmt.Printf("Query result for: %s\n", query)
+	fmt.Printf("Query: %s\n", input)
 
 	for rows.Next() {
-		var id, name, class, subclass, vector string
-		var distance float64
-		if err := rows.Scan(&id, &name, &class, &subclass, &vector, &distance); err != nil {
+		var id, name, class, subclass, alias string
+		var sim float64
+		if err := rows.Scan(&id, &name, &class, &subclass, &alias, &sim); err != nil {
 			log.Fatalf("Failed to scan row: %v", err)
 		}
-		log.Printf("ID: %s, Name: %s, Class: %s, Subclass: %s, Distance: %f",
-			id, name, class, subclass, distance)
+		log.Printf("ID: %s, Name: %s, Class: %s, Subclass: %s, alias: %s, Similarity: %f",
+			id, name, class, subclass, alias, sim)
 
 	}
 
-	log.Printf("Query took %v", timeEnd.Sub(timeStart))
-
+	log.Printf("-----------\n")
+	log.Printf("%v", timeEnd.Sub(timeStart))
 }
 
 func createDB() {
-	processParquet("./data/download/geocodeur_division.geoparquet")
-	processParquet("./data/download/geocodeur_segment.geoparquet")
+	pool := getDBPool()
+	defer pool.Close()
+
+	err := createTableOverture(pool)
+	if err != nil {
+		log.Fatalf("Failed to recreate table: %v", err)
+	}
+
+	err = createTableSearch(pool)
+	if err != nil {
+		log.Fatalf("Failed to recreate table: %v", err)
+	}
+
+	processParquet(pool, "./data/download/geocodeur_division.geoparquet")
+	processParquet(pool, "./data/download/geocodeur_segment.geoparquet")
+	processParquet(pool, "./data/download/geocodeur_poi.geoparquet")
+
+	fmt.Println("Reindexing tables")
+	err = reindex(pool)
+	if err != nil {
+		log.Fatalf("Failed to reindex table: %v", err)
+	}
 }
 
-func processParquet(path string) {
+func processParquet(pool *pgxpool.Pool, path string) {
 	// Open GeoParquet file
 	fr, err := local.NewLocalFileReader(path)
 	if err != nil {
@@ -126,43 +144,6 @@ func processParquet(path string) {
 	pr.ReadStop()
 	fr.Close()
 
-	// Initialize PostgreSQL connection pool
-
-	pool := getDBPool()
-	defer pool.Close()
-
-	// Drop and recreate tables
-	err = recreateTable(pool)
-	if err != nil {
-		log.Fatalf("Failed to recreate table: %v", err)
-	}
-
-	err = recreateTableEmbeddings(pool)
-	if err != nil {
-		log.Fatalf("Failed to recreate table: %v", err)
-	}
-
-	// Prepare insert statements
-	/* stmtOverture, err := pool.Prepare(context.Background(), "insert_overture", `
-	        INSERT INTO overture (id, name, class, subclass, geom)
-	        VALUES ($1, $2, $3, $4, ST_GeomFromText($5, 4326))
-	    `)
-		if err != nil {
-			log.Fatalf("Failed to prepare statement: %v", err)
-		}
-
-		stmtEmbedding, err := pool.Prepare(context.Background(), "insert_embedding", `
-	        INSERT INTO overture_embeddings (id, vector)
-	        VALUES ($1, $2)
-	    `)
-		if err != nil {
-			log.Fatalf("Failed to prepare statement: %v", err)
-		}
-	*/
-	model := getEmbeddingModel()
-	defer model.Destroy()
-
-	// Use a wait group to wait for all goroutines to finish
 	var wg sync.WaitGroup
 	batchSize := 100
 	numBatches := (numRows + batchSize - 1) / batchSize
@@ -177,86 +158,84 @@ func processParquet(path string) {
 		wg.Add(1)
 		go func(records []Record) {
 			defer wg.Done()
-			//tx, err := pool.Begin(context.Background())
 			if err != nil {
 				log.Fatalf("Failed to begin transaction: %v", err)
 			}
 
+			tx, err := pool.Begin(context.Background())
+			if err != nil {
+				log.Fatalf("Failed to begin transaction: %v", err)
+			}
+			defer tx.Rollback(context.Background())
+
 			for _, rec := range records {
-				// Insert overture record in db
-				//_, err = tx.Exec(context.Background(), "insert_overture", rec.ID, rec.Name, rec.Class, rec.Subclass, rec.Geom)
-				_, err := pool.Exec(context.Background(), `insert into overture (id, name, class, subclass, geom) values ($1, $2, $3, $4, ST_GeomFromText($5, 4326))`, rec.ID, rec.Name, rec.Class, rec.Subclass, rec.Geom)
+				err := addOvertureFeature(tx, rec)
 
 				if err != nil {
-					log.Printf("Failed to insert record (ID: %s): %v", rec.ID, err)
+					log.Printf("Failed to insert record: %v", err)
 				}
 
-				// Insert embeddings
+				// Insert alias for name + relation
 				if len(rec.Relation) > 0 {
-					// split relations by ;
 					relations := strings.Split(rec.Relation, ";")
 					for _, relation := range relations {
-						embedding := rec.Name + " " + relation
-						vector := createEmbedding(model, embedding)
-						vectorString := vectorToString(vector)
+						alias := rec.Name + " " + relation
+						addAlias(tx, rec.ID, alias)
 
-						// Insert into database
-						//_, err = tx.Exec(context.Background(), "insert_embedding", rec.ID, vectorString)
-						_, err = pool.Exec(context.Background(), `insert into overture_embeddings (id, vector) values ($1, $2)`, rec.ID, vectorString)
-						if err != nil {
-							log.Printf("Failed to insert record (ID: %s): %v", rec.ID, err)
+						// Add entry for relation aliases
+						for name, alias := range aliases {
+							if relation == name {
+								aliasEmbedding := rec.Name + " " + alias
+								addAlias(tx, rec.ID, aliasEmbedding)
+							}
 						}
 					}
 				}
 
-				embedding := rec.Name
-				vector := createEmbedding(model, embedding)
-				vectorString := vectorToString(vector)
+				// Add name as alias
+				addAlias(tx, rec.ID, rec.Name)
 
-				// Insert into database
-				_, err = pool.Exec(context.Background(), `insert into overture_embeddings (id, vector) values ($1, $2)`, rec.ID, vectorString)
-				if err != nil {
-					log.Printf("Failed to insert record (ID: %s): %v", rec.ID, err)
+				// Add aliases for name aliases
+				for name, alias := range aliases {
+					if rec.Name == name {
+						addAlias(tx, rec.ID, alias)
+					}
 				}
+
+				// Add embedding for truncated names
+				for _, truncation := range truncations {
+					if strings.Contains(rec.Name, truncation) {
+						alias := strings.Trim(strings.Replace(rec.Name, truncation, "", 1), " ")
+						addAlias(tx, rec.ID, alias)
+					}
+				}
+
 			}
 
-			/* if err := tx.Commit(context.Background()); err != nil {
+			if err := tx.Commit(context.Background()); err != nil {
 				log.Fatalf("Failed to commit transaction: %v", err)
-			} */
+			}
+
 		}(records[start:end])
 	}
 
-	// Wait for all goroutines to finish
 	wg.Wait()
+
+	fmt.Printf("Processed %s\n", path)
 }
 
-func vectorToString(vector []float32) string {
-	return fmt.Sprintf("[%s]", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(vector)), ","), "[]"))
+func addOvertureFeature(tx pgx.Tx, rec Record) error {
+	query := fmt.Sprintf(`INSERT INTO %s (id, name, class, subclass, geom) VALUES ($1, $2, $3, $4, ST_GeomFromText($5, 4326))`, TABLEOVERTURE)
+	_, err := tx.Exec(context.Background(), query, rec.ID, rec.Name, rec.Class, rec.Subclass, rec.Geom)
+
+	return err
 }
 
-func getEmbeddingModel() *fastembed.FlagEmbedding {
-	options := fastembed.InitOptions{
-		Model:     fastembed.AllMiniLML6V2,
-		CacheDir:  "model_cache",
-		MaxLength: 200,
-	}
+func addAlias(tx pgx.Tx, id, alias string) error {
+	query := fmt.Sprintf(`INSERT INTO %s (id, alias) VALUES ($1, $2)`, TABLESEARCH)
+	_, err := tx.Exec(context.Background(), query, id, alias)
 
-	model, err := fastembed.NewFlagEmbedding(&options)
-	if err != nil {
-		log.Fatalf("Failed to initialize fastembed model: %v", err)
-	}
-
-	return model
-}
-
-func createEmbedding(model *fastembed.FlagEmbedding, data string) []float32 {
-	doc := []string{data}
-	vector, err := model.Embed(doc, 64)
-	if err != nil {
-		log.Fatalf("Failed to generate vector: %v", data)
-	}
-
-	return vector[0]
+	return err
 }
 
 func getDBPool() *pgxpool.Pool {
@@ -270,35 +249,50 @@ func getDBPool() *pgxpool.Pool {
 	return pool
 }
 
-// Recreate the table in PostgreSQL
-func recreateTableEmbeddings(pool *pgxpool.Pool) error {
-	_, err := pool.Exec(context.Background(), `
-		CREATE EXTENSION IF NOT EXISTS vector;
-		DROP TABLE IF EXISTS overture_embeddings;
-		CREATE TABLE overture_embeddings (
-			id TEXT,
-			vector VECTOR(384)
-		);
+func reindex(pool *pgxpool.Pool) error {
+	query := fmt.Sprintf(`
+		REINDEX TABLE %s;
+		REINDEX TABLE %s;
+	`, TABLEOVERTURE, TABLESEARCH)
 
-		--ALTER TABLE overture_embeddings ADD CONSTRAINT fk_overture_id FOREIGN KEY (id) REFERENCES overture(id) ON DELETE CASCADE;
-		CREATE INDEX vector_idx ON overture_embeddings USING hnsw (vector vector_l2_ops);
-	`)
+	_, err := pool.Exec(context.Background(),
+		query)
 	return err
 }
 
-func recreateTable(pool *pgxpool.Pool) error {
-	_, err := pool.Exec(context.Background(), `
+func createTableOverture(pool *pgxpool.Pool) error {
+	query := fmt.Sprintf(`
 		CREATE EXTENSION IF NOT EXISTS postgis;
-		DROP TABLE IF EXISTS overture;
-		CREATE TABLE overture (
-			id TEXT,
+		DROP TABLE IF EXISTS %[1]s;
+
+		CREATE TABLE %[1]s (
+			id VARCHAR(32) PRIMARY KEY,
 			name TEXT,
 			class TEXT,
 			subclass TEXT,
 			geom geometry(Geometry, 4326)
 		);
+	`, TABLEOVERTURE)
 
-		CREATE INDEX idx_overture_id ON overture (id);
-	`)
+	_, err := pool.Exec(context.Background(), query)
+	return err
+}
+
+// Recreate the table in PostgreSQL
+func createTableSearch(pool *pgxpool.Pool) error {
+	query := fmt.Sprintf(`
+        CREATE EXTENSION IF NOT EXISTS pg_trgm;
+        DROP TABLE IF EXISTS %[1]s;
+        DROP INDEX IF EXISTS idx_%[1]s_trgm;
+
+        CREATE TABLE %[1]s (
+            id VARCHAR(32),
+            alias TEXT
+        );
+
+        CREATE INDEX IF NOT EXISTS idx_%[1]s_trgm ON %[1]s USING gin (alias gin_trgm_ops);
+    `, TABLESEARCH)
+
+	_, err := pool.Exec(context.Background(), query)
 	return err
 }
